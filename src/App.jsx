8ef22f8a1fc8597d96c473f660618a486d3513dc
Jason Cassella <jasoncassella1@gmail.com>
2023-01-23 09:48:14 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState} from 'react';
+
+function App() {
+	const [excuse, setExcuse] = useState('')
+
+	const fetchExcuse = (category) => {
+		fetch(`https://excuser-three.vercel.app/v1/excuse/${category}`)
+			.then(res => res.json())
+			.then(data => setExcuse(data[0].excuse));
+	};
+
+	return (
+		<div className='App'>
+			<h1>Generate An Excuse</h1>
+			<button onClick={() => fetchExcuse('party')}>Party</button>
+			<button onClick={() => fetchExcuse('family')}>Family</button>
+			<button onClick={() => fetchExcuse('office')}>Office</button>
+			{excuse && <p>{excuse}</p>}
+		</div>
+	);
+}
+
+export default App;
